bee727d3b5b26462881c564a618813ae8b323041
Nazarii <nazarisaviak@gmail.com>
2024-01-10 13:00:55 +0200

    fix searchfield

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,7 @@ export const App = () => {
                         className="delete"
                         onClick={() => {
                           document.getElementById('searchID').value = '';
+                          setInputField('');
                         }}
                       />
                     </span>
